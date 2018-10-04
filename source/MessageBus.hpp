cab500467c6d54bb699696999f844b887a67701b
whichxjy <531195874@qq.com>
2018-10-04 21:18:44 +0800

    修复bug，并增加测试代码。

--- source/MessageBus.hpp
+++ source/MessageBus.hpp
@@ -1,9 +1,13 @@
 #ifndef MESSAGE_BUS_HPP
 #define MESSAGE_BUS_HPP
 
+#include "FunctionTraits.hpp"
+#include <iostream>
 #include <string>
 #include <map>
 #include <boost/any.hpp>
+
+using namespace func_traits;
 
 class MessageBus {
 private:
@@ -27,19 +31,57 @@ public:
 
 	//Add message to message bus
 	template <typename F>
-	void AddMsg(F&& func, const std::string& topic = "");
+	void AddMsg(F&& func, const std::string& topic = "") {
+		auto funcWrapper = ToFuncWrapper(std::forward<F>(func));
+		using function_wrapper_type = typename FunctionTraits<F>::function_wrapper_type;
+		std::string tag = topic + typeid(funcWrapper).name();
+		container.emplace(std::move(tag), std::forward<function_wrapper_type>(funcWrapper));
+	}	
 
 	//Delete Message(s) in Message Bus
 	template <typename RetType, typename... Args>
-	void RemoveMsg(const std::string& topic = "");
+	void RemoveMsg(const std::string& topic = "") {
+		using function_wrapper_type = std::function<RetType(Args...)>;
+		std::string removeTag = topic + typeid(function_wrapper_type).name();
+		auto removeRange = container.equal_range(removeTag);
+		if (removeRange.first == removeRange.second)
+			std::cout << "Message not found." << std::endl;
+		else
+			container.erase(removeRange.first, removeRange.second);
+	}
 
 	//Send message(s) without argument
 	template <typename RetType>
-	void SendMsg(const std::string& topic = "");
+	void SendMsg(const std::string& topic = "") {
+		using function_wrapper_type = std::function<RetType()>;
+		std::string sendTag = topic + typeid(function_wrapper_type).name();
+		auto sendRange = container.equal_range(sendTag);
+		if (sendRange.first == sendRange.second) {
+			std::cout << "Message not found." << std::endl;
+			return;
+		}
+		for (Iter it = sendRange.first; it != sendRange.second; ++it) {
+			function_wrapper_type funcWrapper = boost::any_cast<function_wrapper_type>(it->second);
+			funcWrapper();
+		}
+	}
+
 
 	//Send message(s) with argument(s)
 	template <typename RetType, typename... Args>
-	void SendMsg(Args&&... args, const std::string& topic = "");
+	void SendMsg(Args&&... args, const std::string& topic = "") {
+		using function_wrapper_type = std::function<RetType(Args...)>;
+		std::string sendTag = topic + typeid(function_wrapper_type).name();
+		auto sendRange = container.equal_range(sendTag);
+		if (sendRange.first == sendRange.second) {
+			std::cout << "Message not found." << std::endl;
+			return;
+		}
+		for (Iter it = sendRange.first; it != sendRange.second; ++it) {
+			function_wrapper_type funcWrapper = boost::any_cast<function_wrapper_type>(it->second);
+			funcWrapper(std::forward<Args>(args)...);
+		}
+	}
 
 };
 
--- source/main.cc
+++ source/main.cc
@@ -1,11 +1,76 @@
-#include <iostream>
-#include <functional>
-#include <string>
+/****************************
+ *							*
+ *		Just for test		*
+ *							*
+ ****************************/
+
 #include "MessageBus.hpp"
-#include "FunctionTraits.hpp"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+MessageBus MsgBus;
+const string TopicA = "Pink Floyd";
+const string TopicB = "Gorillaz";
+const string CallBackTopic = "Get Message";
+
+class Publisher {
+public:
+	Publisher() {
+		MsgBus.AddMsg([this](const string& callBackMsg){ GetCallBack(callBackMsg); }, CallBackTopic);
+	}
+
+	void SendMsg(const string& topic) {
+		//Publish
+		if (topic == TopicA)
+			MsgBus.SendMsg<void, const string&>("Another Brick In The Wall", TopicA);
+		else if (topic == TopicB)
+			MsgBus.SendMsg<void, const string&>("Tomorrow Comes Today", TopicB);
+	}
+
+	void GetCallBack(const string& callBackMsg) {
+		cout << callBackMsg << endl;
+	}
+};
+
+class Subscriber {
+private:
+	string myName;
+
+public:
+	Subscriber(const string& name) {
+		myName = name;
+	}
+
+	void Subscribe(const string& topic = "") {
+		MsgBus.AddMsg([this](const string& song){ GetMsg(song); }, topic);
+	}
+
+	void GetMsg(const string& song) {
+		//Get Message
+		cout << myName << " get message: " << song << endl;
+		//Call back
+		MsgBus.SendMsg<void, const string&>("Nice song!", CallBackTopic);
+	}
+};
+
 
 int main() {
+	Publisher pub;
+	Subscriber subA("SubscriberA");
+	Subscriber subB("SubscriberB");
 
+	subA.Subscribe(TopicA);
+	subA.Subscribe(TopicB);
+
+	subB.Subscribe(TopicA);
+	subB.Subscribe(TopicB);
+
+	pub.SendMsg(TopicA);
+	pub.SendMsg(TopicB);
 
 	return 0;
 }
+
+
